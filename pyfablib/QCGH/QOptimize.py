7e29e1ec2717615f9089c3d8b96eb3de07ac60be
claire0418 <64419346+claire0418@users.noreply.github.com>
2022-09-27 11:29:34 -0400

    Update QOptimize.py

--- pyfablib/QCGH/QOptimize.py
+++ pyfablib/QCGH/QOptimize.py
@@ -59,15 +59,15 @@ class QOptimize(CGH):
         Vm = np.array(self.Vm)
         delta = np.array(self.delta)
 
-        w = np.ones((len(iterations)+1,len(Vm)))
+        w = np.ones(len(Vm))
         phi = np.zeros((480,640))
 
         for k in iterations:
             for m in range(0,len(Vm)):
-                w[k+1][m] = w[k][m]*(Vm_avg()/abs(Vm[m])
+                w[m] = w[m]*(Vm_avg()/abs(Vm[m]))
                 for x in range(0,480):
                     for y in range(0,640):
-                        phi[x][y] += np.angle(np.exp(1j*delta[m][x][y]*w[k+1][m])*(Vm[m]/abs(Vm[m])))
+                        phi[x][y] += np.angle(np.exp(1j*delta[m][x][y]*w[m])*(Vm[m]/abs(Vm[m])))
             recalculate_Vm(phi, traps)
 				
         self.recalculate.emit(phi)
