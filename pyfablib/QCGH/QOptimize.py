18d1e3ad3468e89b79db3a742414f5b089afd9a2
claire0418 <64419346+claire0418@users.noreply.github.com>
2022-10-03 14:47:16 -0400

    Update QOptimize.py

--- pyfablib/QCGH/QOptimize.py
+++ pyfablib/QCGH/QOptimize.py
@@ -86,8 +86,8 @@ class QOptimize(CGH):
         iterations = np.arange(0,2)
         self.compile_delta(traps)
         delta = np.array(self.delta)
-        #phi_init = self.phi_init(delta)
-        self.recalculate_Vm(self.phi, traps)
+        phi_init = self.phi_init(delta)
+        self.recalculate_Vm(phi_init, traps)
         Vm = np.array(self.Vm)
 
 
