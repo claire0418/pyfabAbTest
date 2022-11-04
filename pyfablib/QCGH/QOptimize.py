60e318de87be1c2098cb56f9226230e15b6f3ede
claire0418 <64419346+claire0418@users.noreply.github.com>
2022-11-04 16:10:24 -0400

    Update QOptimize.py

--- pyfablib/QCGH/QOptimize.py
+++ pyfablib/QCGH/QOptimize.py
@@ -71,8 +71,8 @@ class QOptimize(CGH):
         e = np.full(self.shape,np.e)
         psi = np.zeros(self.shape,dtype='complex_')
         for m in range(len(delta)):
-            #random = 2*np.pi*np.random.rand(self.shape[0], self.shape[1])
-            psi += np.power(e,1j*(delta[m]))
+            random = 2*np.pi*np.random.rand(self.shape[0], self.shape[1])
+            psi += np.power(e,1j*(delta[m]+random))
         phi = np.angle(psi)
         return phi  
 
